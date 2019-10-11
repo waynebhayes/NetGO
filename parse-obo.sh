b2e3e31d60688148236f8b0f49f14a59dda7dc70
Wayne Hayes <whayes@uci.edu>
2019-10-11 10:40:15 -0700

    now correctly prints all paths from every term to its root

--- parse-obo.sh
+++ parse-obo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-awk '{gsub("!.*$","")} # delete all comments
+awk '{gsub("!.*$","")  # delete all comments
+	gsub("GO:","") # when you remove the GO: part it fits in 7 columns so tabs always move just 1 tabstop.
+    }
     /^id:/{id=$2;ID[$2]=1}
     /^is_a:/{P[id][$2]=1}
     /^name:/{gsub("name: ","");gsub(" ","_");name[id]=$0}
@@ -8,18 +10,24 @@ awk '{gsub("!.*$","")} # delete all comments
     /^is_obsolete:/{Ob[id]=1}
     /^.Typedef/{exit} # this marks the end of the actual heirarchy definition
 
-    # Print the path STARTING at the root, down to (but not including) g
-    function PrintPaths(g, p,d){
-	if(isarray(P[g]))for(p in P[g]){
-	    d=PrintPaths(p)
-	    printf "%s(%d)\t",p,d
-	    return d+1
-	} else return 0
+    # Print the path to the root, starting at g
+    function RootPaths(g,d,	p,i,n){
+	printf "%s",g
+	if(isarray(P[g])) {
+	    n=0;
+	    for(p in P[g]){
+		if(n>0)for(i=0;i<d;i++)printf "\t"
+		printf "\t"
+		RootPaths(p,d+1)
+		n++
+	    }
+	} else {
+	    print ""
+	}
     }
 
     END{
 	for(g in ID) if(!Ob[g]) { # if not obsolete
-	    d=PrintPaths(g)
-	    printf "%s(%d)\n",g,d
+	    RootPaths(g,0)
 	}
     }' "$@"
